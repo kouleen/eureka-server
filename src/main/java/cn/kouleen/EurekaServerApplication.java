63d2e227146c6f0846bfd3a83c6aac855bb310fa
zhangqing <ex_qing1.zhang>
2022-01-06 15:07:11 +0800

    - eureka服务端

---
+++ src/main/java/cn/kouleen/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.kouleen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhangqing
+ * @since 2022/1/6 14:28
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
